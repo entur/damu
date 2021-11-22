e50e43914cfd654c250fa73591f6a2417a60c5c1
vpaturet <46598384+vpaturet@users.noreply.github.com>
2021-11-22 12:24:22 +0100

    Code cleanup (#63)

--- src/test/java/no/entur/damu/netex/EnturGtfsExportTest.java
+++ src/test/java/no/entur/damu/netex/EnturGtfsExportTest.java
@@ -34,21 +34,29 @@
 
 package no.entur.damu.netex;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.io.IOUtils;
 import org.entur.netex.gtfs.export.GtfsExporter;
 import org.entur.netex.gtfs.export.stop.DefaultStopAreaRepository;
-import org.apache.commons.io.IOUtils;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.zeroturnaround.zip.ZipUtil;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Path;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.StandardCopyOption;
 
+/**
+ * Coarse-grained integration test that verifies that the GTFS file is generated.
+ */
 class EnturGtfsExportTest {
 
     @Test
-    @Disabled
     void testEnturExport() throws IOException {
 
         DefaultStopAreaRepository defaultStopAreaRepository = new DefaultStopAreaRepository();
@@ -56,18 +64,39 @@ class EnturGtfsExportTest {
 
         InputStream netexTimetableDataset = getClass().getResourceAsStream("/rb_flb-aggregated-netex.zip");
 
-        GtfsExporter gtfsExport = new EnturGtfsExporter("FLB", defaultStopAreaRepository);
+        String codespace = "FLB";
+        GtfsExporter gtfsExport = new EnturGtfsExporter(codespace, defaultStopAreaRepository);
 
 
         InputStream exportedGtfs = gtfsExport.convertTimetablesToGtfs(netexTimetableDataset);
 
+        File gtfsFile = new File("export-gtfs.zip");
         java.nio.file.Files.copy(
                 exportedGtfs,
-                Path.of("export-gtfs-entur.zip"),
+                gtfsFile.toPath(),
                 StandardCopyOption.REPLACE_EXISTING);
+
+        checkAgency(gtfsFile, codespace);
 
         IOUtils.closeQuietly(exportedGtfs);
     }
 
+    private void checkAgency(File gtfsFile, String codespace) throws IOException {
+        Iterable<CSVRecord> records = getCsvRecords(gtfsFile, "agency.txt");
+        Assertions.assertTrue(records.iterator().hasNext());
+        CSVRecord record = records.iterator().next();
+        Assertions.assertNotNull(record.get("agency_id"));
+        Assertions.assertTrue(record.get("agency_id").startsWith(codespace + ':' + "Authority"));
+        Assertions.assertFalse(records.iterator().hasNext());
+    }
+
+
+    private Iterable<CSVRecord> getCsvRecords(File gtfsFile, String entryName) throws IOException {
+        Assertions.assertTrue(ZipUtil.containsEntry(gtfsFile, entryName));
+
+        byte[] zipEntry = ZipUtil.unpackEntry(gtfsFile, entryName, StandardCharsets.UTF_8);
+        CSVFormat csvFormat = CSVFormat.DEFAULT.builder().setHeader().setSkipHeaderRecord(true).build();
+        return csvFormat.parse(new InputStreamReader(new ByteArrayInputStream(zipEntry)));
+    }
 
 }
