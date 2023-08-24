ff877d12ac4e947fd444204e52fb7d7731c2a493
Mansoor Sajjad <mansoor.sajjad@entur.org>
2023-08-24 14:18:50 +0200

    Adding the log statements.

--- src/main/java/no/entur/damu/gtfsconverter/QuayFetcher.java
+++ src/main/java/no/entur/damu/gtfsconverter/QuayFetcher.java
@@ -2,11 +2,16 @@ package no.entur.damu.gtfsconverter;
 
 import org.entur.netex.gtfs.export.exception.QuayNotFoundException;
 import org.rutebanken.netex.model.Quay;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class QuayFetcher extends EnturNetexEntityFetcher<Quay, String> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(QuayFetcher.class);
+
     @Override
     public Quay tryFetch(String quayId) {
+        LOGGER.info("Trying to fetch the Quay with id {}, from read API", quayId);
         try {
             return this.webClient.get()
                     .uri("/quays/{quayId}", quayId)
--- src/main/java/no/entur/damu/gtfsconverter/StopPlaceFetcher.java
+++ src/main/java/no/entur/damu/gtfsconverter/StopPlaceFetcher.java
@@ -2,19 +2,25 @@ package no.entur.damu.gtfsconverter;
 
 import org.entur.netex.gtfs.export.exception.StopPlaceNotFoundException;
 import org.rutebanken.netex.model.StopPlace;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class StopPlaceFetcher extends EnturNetexEntityFetcher<StopPlace, String> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StopPlaceFetcher.class);
     @Override
-    public StopPlace tryFetch(String quayId) {
+    public StopPlace tryFetch(String stopPlaceId) {
+
+        LOGGER.info("Trying to fetch the stop place with id {}, from read API", stopPlaceId);
+
         try {
             return this.webClient.get()
-                    .uri("/quays/{quayId}/stop-place", quayId)
+                    .uri("/quays/{quayId}/stop-place", stopPlaceId)
                     .retrieve()
                     .bodyToMono(StopPlace.class)
                     .block();
         } catch (Exception e) {
-            throw new StopPlaceNotFoundException("Could not find StopPlace for quay id " + quayId + " due to " + e.getMessage());
+            throw new StopPlaceNotFoundException("Could not find StopPlace for quay id " + stopPlaceId + " due to " + e.getMessage());
         }
     }
 }
